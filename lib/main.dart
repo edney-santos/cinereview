0016e716f6e40a55872011e5194925fb353ff82f
ed-cavalcanti <edney2809@gmail.com>
2023-08-09 13:27:09 -0300

    [feat] adicona chave da api tmdb por dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,15 @@ import 'package:cinereview/app/data/repositories/users_repository.dart';
 import 'package:cinereview/app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
